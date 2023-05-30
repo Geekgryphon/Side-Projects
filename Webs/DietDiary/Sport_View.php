930e3e6ba033160beeea972ca96e6c7d598663df
Geekgryphon <geekgryphon@gmail.com>
2023-05-31 07:24:52 +0800

    Push

---
+++ Webs/DietDiary/Sport_View.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="Chest" class="SportTabs">
+        <h3>胸</h3>
+        <div class="ChestVideo1">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/2yuv431SthQ" 
+                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen></iframe>
+        </div>
+    </div>
+    <div id="Back" class="SportTabs">
+        <h3>背</h3>
+    </div>
+    <div id="Leg" class="SportTabs">
+        <h3>腿</h3>
+        <div class="LegVideo1">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/LWdhO_OMrHk" 
+                title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowfullscreen></iframe>
+        </div>
+    </div>
+    <div id="Shoulder" class="SportTabs">
+        <h3>肩</h3>
+    </div>
+    <div id="Arm" class="SportTabs">
+        <h3>手</h3>
+    </div>
+</body>
+</html>
